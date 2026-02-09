0fc7e89f7a893e55a53fc721d6503ad4efb40c94
Rodrigo Kunzler Weber <rodrigo.weber@appmax.com.br>
2026-02-09 17:42:18 -0300

    feat: Add GET endpoint to retrieve all students and refactor database connection logic in index.php

--- php/index.php
+++ php/index.php
@@ -14,13 +14,7 @@ if ($method === 'POST' && $uri === '/aluno/create') {
         exit;
     }
 
-    try {
-        $pdo = new PDO('pgsql:host=127.0.0.1;port=5432;dbname=jiuflix', 'maxter', 'admin', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-    } catch (Exception $e) {
-        http_response_code(500);
-        echo json_encode(['error' => 'Internal server error']);
-        exit;
-    }
+    $pdo = connectionDatabase();
 
     $sql = "INSERT INTO aluno (name, type_graduation) VALUES (:name, :type_graduation)";
 
@@ -32,6 +26,37 @@ if ($method === 'POST' && $uri === '/aluno/create') {
     http_response_code(201);
     echo json_encode(['message' => 'Aluno criado com sucesso']);
     exit;
+}
+
+if ($method === "GET" && $uri === '/alunos') {
+    $pdo = connectionDatabase();
+    $sql = "SELECT * FROM aluno";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+    $array = $stmt->fetchALL(PDO::FETCH_OBJ);
+
+    http_response_code(200);
+    echo json_encode(['Alunos' => $array, 'message' => 'Alunos retornados com sucesso']);
+    exit;
+}
+
+function debug($param) {
+    print_r('<pre>');
+    var_dump($param);
+    print_r('</pre>');
+    die();
+} 
+
+function connectionDatabase () {
+    try {
+        return new PDO('pgsql:host=127.0.0.1;port=5432;dbname=jiuflix', 'maxter', 'admin', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    } catch (Exception $e) {
+        http_response_code(500);
+        echo json_encode(['error' => 'Internal server error']);
+        exit;
+    }
 }
 
 http_response_code(404);
